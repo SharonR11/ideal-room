63d4384b075bd131d2d1c9837c0dba2ca0703b46
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-12-04 19:47:38 -0500

    unir

--- app/alquiler/urls.py
+++ app/alquiler/urls.py
@@ -4,11 +4,19 @@ from . import views
 urlpatterns = [
 
     path('', views.menu, name='menu'),
+    # -------- YA ESTA ----
     path('perfil/', views.perfil, name='perfil'),
-    # path('registrar/',views.insertarpersona,name='registrar'),
-    # path('eliminar/<idPersona>/', views.eliminar, name='eliminar'),
-    # path('data/<int:idPersona>',views.datapersona, name='data'),
-    # path('modificar/<int:idPersona>/', views.editar, name='editar'),
-    # path('buscar/',views.buscar,name='buscar'),
-    # path('perfil/<int:idPersona>',views.perfil, name='perfil'),
+
+    # -------- } ----
+    path('registrar/',views.registro,name='registrar'),
+    # -------- YA ESTA ----
+    path('contacto/', views.contactar, name='contacto'),
+    # -------- YA ESTA ----
+    path('nosotros/',views.nosotros, name='nosotros'),
+    # -------- YA ESTA ----
+    path('detalle/', views.detalles, name='detalle'),
+    # -------- YA ESTA ----
+    path('mapa/',views.mapa,name='mapa'),
+    # -------- ----
+    path('calificacion/',views.calificacion, name='calificacion'),
 ]
--- app/alquiler/views.py
+++ app/alquiler/views.py
@@ -4,6 +4,8 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse, HttpRequest
 from django.db.models import Q
 
+
+# -------- YA ESTA ----
 @login_required
 def menu(request):
     # persona_list=Persona.objects.all()
@@ -11,8 +13,36 @@ def menu(request):
     return render(request,'plantilla/menu.html')
     # return render(request,'plantilla/menu.html')
 
-def insertarpersona(request):
-    return redirect('/')
 
+def registro(request):
+    return render(request,'plantilla/perfilusu.html')
+
+
+# -------- YA ESTA ----
 def perfil(request):
     return render(request,'plantilla/perfilusu.html')
+
+
+# -------- YA ESTA ----
+def contactar(request):
+    return render(request,'plantilla/contactanos.html')
+
+# -------- YA ESTA ----
+def nosotros(request):
+    return render(request,'plantilla/nosotros.html')
+
+# -------- YA ESTA ----
+def detalles(request):
+    return render(request,'plantilla/detalle.html')
+
+# -------- YA ESTA ----
+def mapa(request):
+    return render(request,'plantilla/mapa.html')
+
+
+
+def calificacion(request):
+    return render(request,'plantilla/contactanos.html')
+
+def busqueda(request):
+    return render(request,'plantilla/contactanos.html')
